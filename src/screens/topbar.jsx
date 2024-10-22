7762b5de90a95b5056397fa0cad2b4cfb54de12a
benasdom <benasdomd@gmail.com>
2024-10-22 07:44:06 +0000

    allsvg

--- src/screens/topbar.jsx
+++ src/screens/topbar.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cyberearn from '../assets/cyberearn.png'
 import notify from '../assets/notify.png'
 import beginner from '../assets/beginner.png'
+import streak from '../assets/streak.png'
 
 
 export default function Topbar() {
+  const [streakscore, setstreakscore] = useState(0)
   return (
     <>
     <div className="topbar">
   <div className="profile"><img className="topimga" src={beginner} alt="" /></div>
-  <div className="title"><img className="midimg" src={cyberearn} alt="" /><div className="ttext">CyberEarn</div></div>
+  <div className="title"><img className="midimg" src={cyberearn} alt="" /><div className="ttext">CyberEarn<span className='streak'><img src={streak} className='strkimg'/>{streakscore}</span></div></div>
   <div className="notice"><img className="topimg" src={notify} alt="" /></div>
 </div>
     </>
